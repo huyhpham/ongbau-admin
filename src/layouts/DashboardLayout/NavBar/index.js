8623d0b66424df9c62a4f5ba9a10396a3ecaaa80
huyhpham <phhuy.2201@gmail.com>
2020-09-12 10:33:58 +0700

    Update codes

--- src/layouts/DashboardLayout/NavBar/index.js
+++ src/layouts/DashboardLayout/NavBar/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { Link as RouterLink, useLocation, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   Avatar,
   Box,
@@ -10,16 +10,109 @@ import {
   Drawer,
   Hidden,
   List,
-  Typography,
+  Icon,
   makeStyles
 } from '@material-ui/core';
 import {
   BarChart as BarChartIcon,
   Settings as SettingsIcon,
   Edit as EditIcon,
-  Archive as ArchiveIcon
+  Archive as ArchiveIcon,
+  LogOut as LogOutIcon,
 } from 'react-feather';
+import * as appActions from '../../../store/actions/app';
 import NavItem from './NavItem';
+
+const NavBar = ({ onMobileClose, openMobile }) => {
+  const classes = useStyles();
+  const location = useLocation();
+  const userData = useSelector(state => state.app.user);
+  const history = useHistory(),
+    dispatch = useDispatch();
+
+  useEffect(() => {
+    if (openMobile && onMobileClose) {
+      onMobileClose();
+    }
+  }, [location.pathname]);
+
+  const handleLogOut = async () => {
+    await localStorage.clear();
+    await history.push('/');
+    await dispatch(appActions.resetApp());
+  }
+
+  const content = (
+    <Box
+      height="100%"
+      display="flex"
+      flexDirection="column"
+    >
+      <Box p={2}>
+        <List>
+          {userData.roleId === 1 
+            ? items.map((item) => (
+              <NavItem
+                href={item.href}
+                key={item.title}
+                title={item.title}
+                icon={item.icon}
+              />
+            ))
+            : itemsNotAdmin.map((item) => (
+              <NavItem
+                href={item.href}
+                key={item.title}
+                title={item.title}
+                icon={item.icon}
+              />
+            ))
+          }
+            <Button
+              activeClassName={classes.active}
+              className={classes.button}
+              onClick={handleLogOut}
+            >
+              <LogOutIcon
+                className={classes.icon}
+                size="20"
+              />
+              <span className={classes.title}>
+                Đăng xuất
+              </span>
+            </Button>
+        </List>
+      </Box>
+      <Box flexGrow={1} />
+    </Box>
+  );
+
+  return (
+    <>
+      <Hidden lgUp>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.mobileDrawer }}
+          onClose={onMobileClose}
+          open={openMobile}
+          variant="temporary"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+      <Hidden mdDown>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.desktopDrawer }}
+          open
+          variant="persistent"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+    </>
+  );
+};
 
 const items = [
   {
@@ -32,11 +125,6 @@ const items = [
     icon: ArchiveIcon,
     title: 'Thống kê'
   },
-  // {
-  //   href: '/products',
-  //   icon: ShoppingBagIcon,
-  //   title: 'Products'
-  // },
   {
     href: '/patient',
     icon: EditIcon,
@@ -72,7 +160,7 @@ const itemsNotAdmin = [
   }
 ];
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   mobileDrawer: {
     width: 256
   },
@@ -85,78 +173,37 @@ const useStyles = makeStyles(() => ({
     cursor: 'pointer',
     width: 64,
     height: 64
+  },
+  item: {
+    display: 'flex',
+    paddingTop: 0,
+    paddingBottom: 0
+  },
+  button: {
+    color: theme.palette.text.secondary,
+    fontWeight: theme.typography.fontWeightMedium,
+    justifyContent: 'flex-start',
+    letterSpacing: 0,
+    padding: '10px 8px',
+    textTransform: 'none',
+    width: '100%'
+  },
+  icon: {
+    marginRight: theme.spacing(1)
+  },
+  title: {
+    marginRight: 'auto'
+  },
+  active: {
+    color: theme.palette.primary.main,
+    '& $title': {
+      fontWeight: theme.typography.fontWeightMedium
+    },
+    '& $icon': {
+      color: theme.palette.primary.main
+    }
   }
 }));
-
-const NavBar = ({ onMobileClose, openMobile }) => {
-  const classes = useStyles();
-  const location = useLocation();
-  const userData = useSelector(state => state.app.user);
-
-  useEffect(() => {
-    if (openMobile && onMobileClose) {
-      onMobileClose();
-    }
-  }, [location.pathname]);
-
-  const content = (
-    <Box
-      height="100%"
-      display="flex"
-      flexDirection="column"
-    >
-      <Box p={2}>
-        <List>
-          {userData.roleId === 1 
-            ? items.map((item) => (
-              <NavItem
-                href={item.href}
-                key={item.title}
-                title={item.title}
-                icon={item.icon}
-              />
-            ))
-            : itemsNotAdmin.map((item) => (
-              <NavItem
-                href={item.href}
-                key={item.title}
-                title={item.title}
-                icon={item.icon}
-              />
-            ))
-          }
-        </List>
-      </Box>
-      <Box flexGrow={1} />
-    </Box>
-  );
-
-  return (
-    <>
-      <Hidden lgUp>
-        <Drawer
-          anchor="left"
-          classes={{ paper: classes.mobileDrawer }}
-          onClose={onMobileClose}
-          open={openMobile}
-          variant="temporary"
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-      <Hidden mdDown>
-        <Drawer
-          anchor="left"
-          classes={{ paper: classes.desktopDrawer }}
-          open
-          variant="persistent"
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-    </>
-  );
-};
 
 NavBar.propTypes = {
   onMobileClose: PropTypes.func,
